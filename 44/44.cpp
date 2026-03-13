fef464350bc591696f343a52c3c9e5ce7de6e816
chenxixianming <fengwang000000#gmail.com>
2026-03-13 14:00:03 +0800

    44 updated

--- 44/44.cpp
+++ 44/44.cpp
@@ -32,10 +32,67 @@ class Solution
             temp.push_back(i);
         }
         to_match.push_back(temp);
-        int left = 0;
-        while(left < s.size())
-        {
+        int left = 0, right = 0;
+        int idx = 0;
 
+        if(!to_match[0].empty())
+        {
+            for(int i = 0; i < to_match[0].size(); i++)
+            {
+                if(to_match[0][i] == '?')
+                    continue;
+                if(to_match[0][i] == s[i])
+                    continue;
+                return false;
+            }
+            idx = 1;
+        }
+        if(!to_match.back().empty())
+        {
+            auto start = s.size() - to_match[0].size();
+            for(int i = 0; i < to_match[0].size(); i++)
+            {
+                if(to_match[0][i] == '?')
+                    continue;
+                if(to_match[0][i] == s[i + start])
+                    continue;
+                return false;
+            }
+            to_match.pop_back();
+            s.resize(s.size() - to_match.back().size());
+        }
+        while(right < s.size()  && idx < to_match.size())
+        {
+            if(to_match[idx].empty())
+            {
+                if(idx = 0)
+                {
+                    idx++;
+                }
+                else 
+                {
+                    idx++;
+                    break;
+                }
+            }
+            if(left + to_match[idx].size() >= s.size())
+                break;
+            int i = 0;
+            for(i = 0; i < to_match[idx].size(); i++)
+            {
+                if(s[left + i] != to_match[idx][i])
+                {
+                    i--;
+                    break;
+                }
+            }
+            if(i < to_match[idx].size() - 1)
+            {
+                left++;
+                continue;
+            }
+            idx++;
+            left += i + 1;
         }
     }
 };
